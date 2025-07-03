bfd55a0abc3e5ac1c62035d5032072dc71ccbf41
John M Dusel <62239949+johnmdusel@users.noreply.github.com>
2025-07-03 07:47:56 -0700

    MAINT: backend: Simplify HDI calculation, remove test script

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,4 +1,3 @@
-import heapq
 import random
 from typing import List, Tuple, Union
 
@@ -25,33 +24,31 @@ def get_mode(a: float, b: float) -> Union[float, None]:
     return (a - 1) / (a + b - 2)
 
 
-def get_hdi(a: float, b: float, hdi_mass: float) -> Tuple[float, Tuple[float, float]]:
+def get_hdi(a: float, b: float, hdi_mass: float, n_samples=10**5) -> Tuple[float, Tuple[float, float]]:
     """
-    Endpoints of the highest density interval from a beta distribution.
+    Estimated highest density interval from a beta distribution.
 
     :param a: Parameter
     :param b: Parameter
     :param hdi_mass: Confidence level from UI
-    :return: width, (lower_limit, upper_limit) of highest density interval
+    :param n_samples: Number of samples used for estimation
+    :return: Estimated width, (lower_limit, upper_limit) of highest density interval
     """
-    samples = sorted(_sample_beta(a, b, n=10**5))
-    idx_lo = 0
-    idx_hi = int(hdi_mass * len(samples))
-    endpoints = []
-    heapq.heapify(endpoints)
-    while idx_hi < len(samples):
-        heapq.heappush(
-            endpoints,
-        (
-                samples[idx_hi] - samples[idx_lo],  # width
-                (samples[idx_lo], samples[idx_hi])  # (lower_limit, upper_limit)
-            )
-        )
-        idx_lo += 1
-        idx_hi += 1
-    return endpoints[0]
+    samples = sorted(_sample_beta(a, b, n_samples))
+    window_width = int(hdi_mass * len(samples))
+    min_interval_width = float('inf')
+    min_interval_idx_lo = 0
+
+    for idx in range(len(samples) - window_width):
+        cur_interval_width = samples[idx + window_width] - samples[idx]
+        if cur_interval_width < min_interval_width:
+            min_interval_width = cur_interval_width
+            min_interval_idx_lo = idx
+
+    return min_interval_width, (samples[min_interval_idx_lo],
+                                samples[min_interval_idx_lo + window_width])
 
 
-def _sample_beta(a: float, b: float, n: int) -> List[float]:
-    random.seed(42)
+def _sample_beta(a: float, b: float, n: int, seed: int = 42) -> List[float]:
+    random.seed(seed)
     return [random.betavariate(a, b) for _ in range(n)]
